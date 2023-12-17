c997d43ce1e2c3b85070187e3de2d030e23c6af7
oleksandr-ishchuk <alexmway@mail.ru>
2023-12-17 20:40:35 +0100

    add method in controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -9,12 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    #[Route('/test1', name: 'app_test1')]
-
-    public function index(): Response
+    public function test(): Response
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'Привет, первый запуск контроллера',
+            'controller_name' => 'Привет, это метод test',
+        ]);
+    }
+
+    #[Route('/test1', name: 'app_test1')]
+    public function test1(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'Привет, это метод test 1',
         ]);
     }
 }
