84895565eeb6379f0c25ce1c3189cdfc81621782
oleksandr-ishchuk <alexmway@mail.ru>
2023-12-23 20:13:55 +0100

    add request for response

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,24 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
-    public function test(): Response
+    #[Route('/', name: 'app_test')]
+    public function test(Request $request): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'Привет, это метод test',
-        ]);
-    }
+        $rez = '';
 
-    #[Route('/test1', name: 'app_test1')]
-    public function test1(): Response
-    {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'Привет, это метод test 1',
-        ]);
+        $name = $request->query->get('name');
+
+        if ($name) {
+            $rez = 'Привет '.$name.' к сожалению у нас ремонт.';
+        }
+        // return $this->render('test/index.html.twig', [
+        //     'controller_name' => 'Привет, это метод test',
+        // ]);
+        return new Response('
+
+<html>
+  <body>
+    '.$rez.'
+    <img src="/images/repair.png">
+  </body>
+</html>
+
+    ');
     }
 }
